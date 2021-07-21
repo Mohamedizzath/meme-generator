d56d5e5775ee3193d2e421eed56b312419a4bd5a
Mohamedizzath <mohamedizzath2016@gmail.com>
2021-07-21 20:28:06 +0530

    Added main components and list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { CardGroup, Container } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  const listOfMemes = [{name: 'izzath', likes : 0}, {name: 'kamal', likes : 0}, {name: 'nimal', likes : 0}, {name: 'sunil', likes : 0}, {name: 'perera', likes : 0}];
+
+  const listMeme = listOfMemes.map(meme => 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{meme.name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+
   return (
-    <div>Hii</div>
+    <Container style={{height: "100%"}} className="align-middle">
+      <CardGroup>
+        {listMeme}
+      </CardGroup>
+    </Container>
     );
 }
 
