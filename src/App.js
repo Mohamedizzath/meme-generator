85f11ea2c673ad608c91116a4e11acb862cda4d6
Mohamedizzath <mohamedizzath2016@gmail.com>
2021-07-25 11:41:34 +0530

    Added final touch

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 
   const listOfMemes = [
-    {name: 'Sachin', url: '/images/meme1.jpg', comment: ""}, 
-    {name: 'Malindu', url: '/images/meme2.jpg', comment: ""}, 
-    {name: 'Nipun', url: '/images/meme3.jpg', comment: ""}, 
-    {name: 'Sandul', url: '/images/meme4.jpg', comment: ""}, 
-    {name: 'Izzath', url: '/images/meme5.jpg', comment: ""}];
+    {name: 'Naresh', url: '/images/meme1.jpg', comment: "Gym boy"}, 
+    {name: 'Ashfak', url: '/images/meme2.jpg', comment: "Linux fan"}, 
+    {name: 'Nipun', url: '/images/meme3.jpg', comment: "Music for life"}, 
+    {name: 'Sandul', url: '/images/meme4.jpg', comment: "Positive thinking"}, 
+    {name: 'Pubudu', url: '/images/meme5.jpg', comment: "Linux go brr"}];
 
   const listMeme = listOfMemes.map(meme => 
-    <Card style={{ width: '18rem', margin: '10px', marginTop: '15px'}} className="fonts card-background">
+    <Card style={{ width: '18rem', margin: '10px', marginTop: '30px'}} className="fonts card-background">
       <Card.Img variant="top" src={meme.url} thumbnail/>
       <Card.Body>
         <Card.Title><h3>{meme.name}</h3></Card.Title>
         <Card.Text>
-        
+          <Button type="submit" className="button button-rework">
+                    {meme.comment}
+          </Button>
         </Card.Text>
       </Card.Body>
       
